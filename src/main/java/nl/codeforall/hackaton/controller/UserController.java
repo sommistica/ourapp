e1c0d7422a15541e1e898e5ea1b6fe3f78f94128
Royva <roy.van.straten@hotmail.com>
2020-04-22 19:10:34 +0200

    adding user with json show user with json

--- src/main/java/nl/codeforall/hackaton/controller/UserController.java
+++ src/main/java/nl/codeforall/hackaton/controller/UserController.java
@@ -5,11 +5,16 @@ import nl.codeforall.hackaton.dto.UserDto;
 import nl.codeforall.hackaton.model.User;
 import nl.codeforall.hackaton.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
@@ -28,13 +33,33 @@ public class UserController {
         this.dtoConverter = dtoConverter;
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/2")
-    public void showUser(){
-        User user = new User();
-        user.setUsername("gijs");
-        user.setPassword("sfafdafasdfsdf");
-        user.setEmail("gewoongijs@gmail.com");
-        userService.save(user);
+    @RequestMapping(method = RequestMethod.POST, path = {"/", ""})
+    public ResponseEntity<UserDto> addUser(@Valid @RequestBody UserDto userDto, BindingResult bindingResult, UriComponentsBuilder uriComponentsBuilder){
+
+        if(bindingResult.hasErrors() || userDto.getId() != null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        User savedUser = userService.save(dtoConverter.ConvertToUser(userDto));
+
+        UriComponents uriComponents = uriComponentsBuilder.path("/api/user/" + savedUser.getId()).build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<UserDto> showUser(@PathVariable Integer id){
+
+        User user = userService.get(id);
+
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(dtoConverter.ConvertToDto(user), HttpStatus.OK);
     }
 
 }
