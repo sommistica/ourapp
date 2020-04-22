13dd2ffbb0c75eca3911de6ad6dd0e09601e5a89
Royva <roy.van.straten@hotmail.com>
2020-04-22 23:05:53 +0200

    retry

--- src/main/java/nl/codeforall/hackaton/dao/ChallengeDao.java
+++ src/main/java/nl/codeforall/hackaton/dao/ChallengeDao.java
@@ -1,7 +1,9 @@
 package nl.codeforall.hackaton.dao;
 
 import nl.codeforall.hackaton.model.Challenge;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ChallengeDao extends GenericDao<Challenge> {
 
     public ChallengeDao(){
--- src/main/java/nl/codeforall/hackaton/service/ChallengeService.java
+++ src/main/java/nl/codeforall/hackaton/service/ChallengeService.java
@@ -1,7 +1,9 @@
 package nl.codeforall.hackaton.service;
 
 import nl.codeforall.hackaton.model.Challenge;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChallengeService extends GenericService<Challenge> {
 
     public void incrementViewers(Integer id){
